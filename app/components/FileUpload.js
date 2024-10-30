bd2eea9048dec7fdf1054f543ea9996ca750375c
hardik <hardikbhatia777@gmail.com>
2024-10-30 18:57:31 +0530

    chore: changed background

--- app/components/FileUpload.js
+++ app/components/FileUpload.js
@@ -36,7 +36,7 @@ export default function FileUpload({ onUploadComplete }) {
         alert(`ERROR! ${error.message}`);
         }}
         appearance={{
-        button: "ut-ready:bg-blue-500 bg-gray-600 ut-uploading:bg-blue-400 ut-uploaded:bg-green-500 rounded-md border-2 border-gray-300 bg-white px-6 py-3 text-lg font-semibold text-gray-700 shadow-sm transition-all hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 ut-uploading:cursor-not-allowed",
+        button: "ut-ready:bg-blue-500 bg-gray-600 ut-uploading:bg-blue-400 ut-uploaded:bg-green-500 rounded-md border-2 border-gray-300 bg-white px-6 py-3 text-lg font-semibold text-gray-700 shadow-sm transition-all hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 ut-uploading:cursor-not-allowed",
         container: "w-full max-w-md",
         allowedContent: "text-sm text-gray-600 mt-2",
         uploadIcon: "w-6 h-6 text-blue-500 mb-2",
