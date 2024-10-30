877f1cd0070547c7b3ddd38b392a7f5f53fa921c
hardik <hardikbhatia777@gmail.com>
2024-10-30 17:23:42 +0530

    chore: fix typo

--- app/api/uploadthing/core.js
+++ app/api/uploadthing/core.js
@@ -4,7 +4,7 @@ import { createUploadthing } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "2MB" } })
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Upload complete for userId:", metadata.userId);
       console.log("file url", file.url);
